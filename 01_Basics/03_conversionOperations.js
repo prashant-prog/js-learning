7375a3fbb54b8603739bc4e95d7de1ba3a4c15b5
Prashant Sharma <prashant2006sharma@gmail.com>
2025-11-27 19:49:24 +0530

    Basics

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+let score = "33";
+console.log(typeof score);
+let valInNum = Number(score);
+console.log(typeof valInNum);
+console.log(valInNum);
+
+/*  
+"33" => 33
+"33abc" => NaN #Not a Number
+true => 1 and false => 0
+*/
+
+let someNum = 33;
+let StrNum = String(someNum);
+console.log(StrNum);
+console.log(typeof StrNum);
+
+
