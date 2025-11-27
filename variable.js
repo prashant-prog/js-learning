fa6eb26b8d1e26c4403e2aea510fbd583702f693
Prashant Sharma <prashant2006sharma@gmail.com>
2025-11-27 18:22:45 +0530

    Updated files

--- variable.js
+++ variable.js
@@ -1,16 +1,14 @@
 const accountId = 13345
 let accountEmail = "prashant@gmail.com"
 var accountPassword = "12345" // DONT USE THIS
-accountCity = "Jaipur"
-
+accountCity = "Jaipur" // This is allowed in js but dont use this method to declare variables
 /* 
 Dont use var because of issue in block scope and functional scope
 */ 
-
-
 // accountId = 111 // not allowed
 accountEmail="pts@pts.com"
 accountPassword="23132"
 accountCity="Delhi"
-console.log(accountId);
-console.table([accountId, accountEmail, accountPassword]) // output in tabular form
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountState]) // output in tabular form
