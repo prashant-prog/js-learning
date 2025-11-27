51dc0479352f97f3df2297fc68083d7ecda44d93
Prashant Sharma <prashant2006sharma@gmail.com>
2025-11-27 18:19:48 +0530

    Updated files

--- variable.js
+++ variable.js
@@ -1,11 +1,16 @@
 const accountId = 13345
 let accountEmail = "prashant@gmail.com"
-var accountPassword = "12345"
+var accountPassword = "12345" // DONT USE THIS
 accountCity = "Jaipur"
+
+/* 
+Dont use var because of issue in block scope and functional scope
+*/ 
+
 
 // accountId = 111 // not allowed
 accountEmail="pts@pts.com"
 accountPassword="23132"
 accountCity="Delhi"
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassword])
+console.table([accountId, accountEmail, accountPassword]) // output in tabular form
