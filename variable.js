4fea7ed10cd368b6e38a8708a599eb7cb527ff11
Prashant Sharma <prashant2006sharma@gmail.com>
2025-11-27 18:12:58 +0530

    update

---
+++ variable.js
@@ -0,0 +1,11 @@
+const accountId = 13345
+let accountEmail = "prashant@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 111 // not allowed
+accountEmail="pts@pts.com"
+accountPassword="23132"
+accountCity="Delhi"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword])
