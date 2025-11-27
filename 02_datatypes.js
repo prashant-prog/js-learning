3f7e7ccb650104a025131c60e39f006f6b17b49c
Prashant Sharma <prashant2006sharma@gmail.com>
2025-11-27 18:45:50 +0530

    DataTypes

---
+++ 02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+// alert("Hello") // we are using node js, not browser
+
+console.log(3 + 3);
+
+console.log("Prashant Sharma");
+
+let name = "Prashant"; // String Datatype
+let age = 18; // numeric dataype
+let bigint = 893289238938398984374847383n; //bigint stores big integers
+let accountStatus = true;
+accountStatus = false; //These True and false are boolean datatype
+let temp = null; //Null is a empty value
+let number; //It is undefined because it has no value or anything inside this variable
+/* 
+
+One more datatype is 'Symbol' 
+In JavaScript, Symbol is a primitive data type introduced in ES6 (ECMAScript 2015). It represents a unique and immutable identifier. 
+
+
+const sym1 = Symbol('description');
+const sym2 = Symbol('description');
+console.log(sym1 === sym2); // false
+
+*/
+
+console.log(typeof undefined);
+console.log(typeof null);
