8cf64df03724724844e0f0474268f35c1f72fbc5
Prashant Sharma <prashant2006sharma@gmail.com>
2025-11-28 09:36:15 +0530

    Basics

---
+++ 01_Basics/02.1_datatype-summary.js
@@ -0,0 +1,31 @@
+//Primitive Data types
+/*
+
+7 types:
+String
+Number
+Boolean
+Null
+Undefined
+Bigint
+Symbol
+*/
+
+//Reference Types(Non Premitive)
+
+/*
+Arrays
+Objects
+Functions
+*/
+let bigNum = 327632786237623786276n;
+const Arrays = ["A", "B", "C"];
+let myobj = {
+  name: "Prashant",
+  age: 20,
+};
+let myfunc = function () {
+  console.log("Helllo World");
+};
+
+console.log(typeof bigNum);
